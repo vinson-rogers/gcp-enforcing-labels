b31d94dadbcacb283bcfe39d23ca1162d15e2074
Rogers, Vinson <Vinson.Rogers.ctr@sabre.com>
2022-06-07 16:17:43 -0500

    fixed build branch

--- terraform/cloud-build.tf
+++ terraform/cloud-build.tf
@@ -133,7 +133,7 @@ resource "google_cloudbuild_trigger" "c7n-clone-build-deploy" {
   name    = "c7n-clone-build-deploy"
   trigger_template {
     project_id  = var.project_id
-    branch_name = "$main^"
+    branch_name = "^master$"
     repo_name   = "c7n"
   }
   filename = "c7n-pipeline.yaml"
