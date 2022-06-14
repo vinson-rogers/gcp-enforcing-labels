1a6bfdfd02f9140ea6643af745915ab10aa4f730
Rogers, Vinson <Vinson.Rogers.ctr@sabre.com>
2022-06-14 11:55:05 -0500

    api list

--- terraform/apis.tf
+++ terraform/apis.tf
@@ -1,45 +1,6 @@
 
-resource "google_project_service" "enable_resource_manager" {
-  project = var.project_id
-  service = "cloudresourcemanager.googleapis.com"
-}
-
-resource "google_project_service" "enable_source_repo" {
-  project = var.project_id
-  service = "sourcerepo.googleapis.com"
-}
-
-resource "google_project_service" "enable_cloud_build" {
-  project = var.project_id
-  service = "cloudbuild.googleapis.com"
-}
-
-resource "google_project_service" "enable_cloud_run" {
-  project = var.project_id
-  service = "run.googleapis.com"
-}
-
-resource "google_project_service" "enable_artifact_registry" {
-  project = var.project_id
-  service = "artifactregistry.googleapis.com"
-}
-
-resource "google_project_service" "enable_pubsub" {
-  project = var.project_id
-  service = "pubsub.googleapis.com"
-}
-
-resource "google_project_service" "enable_cloud_functions" {
-  project = var.project_id
-  service = "cloudfunctions.googleapis.com"
-}
-
-resource "google_project_service" "enable_scc" {
-  project = var.project_id
-  service = "securitycenter.googleapis.com"
-}
-
-resource "google_project_service" "enable_cloud_scheduler" {
-  project = var.project_id
-  service = "cloudscheduler.googleapis.com"
+resource "google_project_service" "enable_api" {
+  project  = var.project_id
+  for_each = var.apis
+  service  = each.value
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,3 +3,15 @@ variable region {}
 variable project_id {}
 variable function_bucket_name {}
 variable function_location {}
+variable "apis" {
+  type = list(string)
+  default = ["cloudresourcemanager.googleapis.com",
+    "sourcerepo.googleapis.com",
+    "cloudbuild.googleapis.com",
+    "run.googleapis.com",
+    "artifactregistry.googleapis.com",
+    "pubsub.googleapis.com",
+    "cloudfunctions.googleapis.com",
+    "securitycenter.googleapis.com",
+  "cloudscheduler.googleapis.com"]
+}
