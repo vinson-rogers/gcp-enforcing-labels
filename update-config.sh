28041efdfc62f0ee6d50a62e25adaf8b67ea30ef
Rogers, Vinson <Vinson.Rogers.ctr@sabre.com>
2022-06-14 12:07:34 -0500

    initial update-config

---
+++ update-config.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+cp terraform/terraform.tfvars.example terraform/terraform.tfvars
+sed -i '.bak' "s/<ORG_ID>/${ORG_ID}/g" terraform/terraform.tfvars
+sed -i '.bak' "s/<PROJECT_ID>/${PROJECT_ID}/g" terraform/terraform.tfvars
+sed -i '.bak' "s/<PROJECT_ID>/${PROJECT_ID}/g" copy_to_c7n_repo/c7n-pipeline.yaml
